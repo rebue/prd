4e7cbcc99ab9734dbad0466c023f768b4b0759df
lbl596038980 <596038980@qq.com>
2019-05-27 15:39:43 +0800

    添加校验规格编码是否存在的方法（exist）

--- prd-svr/src/main/java/rebue/prd/ctrl/PrdProductSpecCodeCtrl.java
+++ prd-svr/src/main/java/rebue/prd/ctrl/PrdProductSpecCodeCtrl.java
@@ -26,157 +26,187 @@ import rebue.robotech.ro.Ro;
 @RestController
 public class PrdProductSpecCodeCtrl {
 
-    /**
-     * @mbg.generated 自动生成，如需修改，请删除本行
-     */
-    private static final Logger _log = LoggerFactory.getLogger(PrdProductSpecCodeCtrl.class);
+	/**
+	 * @mbg.generated 自动生成，如需修改，请删除本行
+	 */
+	private static final Logger _log = LoggerFactory.getLogger(PrdProductSpecCodeCtrl.class);
 
-    /**
-     * @mbg.generated 自动生成，如需修改，请删除本行
-     */
-    @Resource
-    private PrdProductSpecCodeSvc svc;
+	/**
+	 * @mbg.generated 自动生成，如需修改，请删除本行
+	 */
+	@Resource
+	private PrdProductSpecCodeSvc svc;
 
-    /**
-     * 有唯一约束的字段名称
-     *
-     * @mbg.generated 自动生成，如需修改，请删除本行
-     */
-    private String _uniqueFilesName = "某字段内容";
+	/**
+	 * 有唯一约束的字段名称
+	 *
+	 * @mbg.generated 自动生成，如需修改，请删除本行
+	 */
+	private String _uniqueFilesName = "某字段内容";
 
-    /**
-     * 添加产品规格编码
-     *
-     * @mbg.generated 自动生成，如需修改，请删除本行
-     */
-    @PostMapping("/prd/productspeccode")
-    IdRo add(@RequestBody PrdProductSpecCodeMo mo) throws Exception {
-        _log.info("add PrdProductSpecCodeMo: {}", mo);
-        IdRo ro = new IdRo();
-        try {
-            int result = svc.add(mo);
-            if (result == 1) {
-                String msg = "添加成功";
-                _log.info("{}: mo-{}", msg, mo);
-                ro.setMsg(msg);
-                ro.setResult(ResultDic.SUCCESS);
-                ro.setId(mo.getId().toString());
-                return ro;
-            } else {
-                String msg = "添加失败";
-                _log.error("{}: mo-{}", msg, mo);
-                ro.setMsg(msg);
-                ro.setResult(ResultDic.FAIL);
-                return ro;
-            }
-        } catch (DuplicateKeyException e) {
-            String msg = "添加失败，" + _uniqueFilesName + "已存在，不允许出现重复";
-            _log.error(msg + ": mo-" + mo, e);
-            ro.setMsg(msg);
-            ro.setResult(ResultDic.FAIL);
-            return ro;
-        } catch (RuntimeException e) {
-            String msg = "添加失败，出现运行时异常";
-            _log.error(msg + ": mo-" + mo, e);
-            ro.setMsg(msg);
-            ro.setResult(ResultDic.FAIL);
-            return ro;
-        }
-    }
+	/**
+	 * 添加产品规格编码
+	 *
+	 * @mbg.generated 自动生成，如需修改，请删除本行
+	 */
+	@PostMapping("/prd/productspeccode")
+	IdRo add(@RequestBody PrdProductSpecCodeMo mo) throws Exception {
+		_log.info("add PrdProductSpecCodeMo: {}", mo);
+		IdRo ro = new IdRo();
+		try {
+			int result = svc.add(mo);
+			if (result == 1) {
+				String msg = "添加成功";
+				_log.info("{}: mo-{}", msg, mo);
+				ro.setMsg(msg);
+				ro.setResult(ResultDic.SUCCESS);
+				ro.setId(mo.getId().toString());
+				return ro;
+			} else {
+				String msg = "添加失败";
+				_log.error("{}: mo-{}", msg, mo);
+				ro.setMsg(msg);
+				ro.setResult(ResultDic.FAIL);
+				return ro;
+			}
+		} catch (DuplicateKeyException e) {
+			String msg = "添加失败，" + _uniqueFilesName + "已存在，不允许出现重复";
+			_log.error(msg + ": mo-" + mo, e);
+			ro.setMsg(msg);
+			ro.setResult(ResultDic.FAIL);
+			return ro;
+		} catch (RuntimeException e) {
+			String msg = "添加失败，出现运行时异常";
+			_log.error(msg + ": mo-" + mo, e);
+			ro.setMsg(msg);
+			ro.setResult(ResultDic.FAIL);
+			return ro;
+		}
+	}
 
-    /**
-     * 修改产品规格编码
-     *
-     * @mbg.generated 自动生成，如需修改，请删除本行
-     */
-    @PutMapping("/prd/productspeccode")
-    Ro modify(@RequestBody PrdProductSpecCodeMo mo) throws Exception {
-        _log.info("modify PrdProductSpecCodeMo: {}", mo);
-        Ro ro = new Ro();
-        try {
-            if (svc.modify(mo) == 1) {
-                String msg = "修改成功";
-                _log.info("{}: mo-{}", msg, mo);
-                ro.setMsg(msg);
-                ro.setResult(ResultDic.SUCCESS);
-                return ro;
-            } else {
-                String msg = "修改失败";
-                _log.error("{}: mo-{}", msg, mo);
-                ro.setMsg(msg);
-                ro.setResult(ResultDic.FAIL);
-                return ro;
-            }
-        } catch (DuplicateKeyException e) {
-            String msg = "修改失败，" + _uniqueFilesName + "已存在，不允许出现重复";
-            _log.error(msg + ": mo=" + mo, e);
-            ro.setMsg(msg);
-            ro.setResult(ResultDic.FAIL);
-            return ro;
-        } catch (RuntimeException e) {
-            String msg = "修改失败，出现运行时异常";
-            _log.error(msg + ": mo-" + mo, e);
-            ro.setMsg(msg);
-            ro.setResult(ResultDic.FAIL);
-            return ro;
-        }
-    }
+	/**
+	 * 修改产品规格编码
+	 *
+	 * @mbg.generated 自动生成，如需修改，请删除本行
+	 */
+	@PutMapping("/prd/productspeccode")
+	Ro modify(@RequestBody PrdProductSpecCodeMo mo) throws Exception {
+		_log.info("modify PrdProductSpecCodeMo: {}", mo);
+		Ro ro = new Ro();
+		try {
+			if (svc.modify(mo) == 1) {
+				String msg = "修改成功";
+				_log.info("{}: mo-{}", msg, mo);
+				ro.setMsg(msg);
+				ro.setResult(ResultDic.SUCCESS);
+				return ro;
+			} else {
+				String msg = "修改失败";
+				_log.error("{}: mo-{}", msg, mo);
+				ro.setMsg(msg);
+				ro.setResult(ResultDic.FAIL);
+				return ro;
+			}
+		} catch (DuplicateKeyException e) {
+			String msg = "修改失败，" + _uniqueFilesName + "已存在，不允许出现重复";
+			_log.error(msg + ": mo=" + mo, e);
+			ro.setMsg(msg);
+			ro.setResult(ResultDic.FAIL);
+			return ro;
+		} catch (RuntimeException e) {
+			String msg = "修改失败，出现运行时异常";
+			_log.error(msg + ": mo-" + mo, e);
+			ro.setMsg(msg);
+			ro.setResult(ResultDic.FAIL);
+			return ro;
+		}
+	}
 
-    /**
-     * 删除产品规格编码
-     *
-     * @mbg.generated 自动生成，如需修改，请删除本行
-     */
-    @DeleteMapping("/prd/productspeccode")
-    Ro del(@RequestParam("id") java.lang.Long id) {
-        _log.info("del PrdProductSpecCodeMo by id: {}", id);
-        int result = svc.del(id);
-        Ro ro = new Ro();
-        if (result == 1) {
-            String msg = "删除成功";
-            _log.info("{}: id-{}", msg, id);
-            ro.setMsg(msg);
-            ro.setResult(ResultDic.SUCCESS);
-            return ro;
-        } else {
-            String msg = "删除失败，找不到该记录";
-            _log.error("{}: id-{}", msg, id);
-            ro.setMsg(msg);
-            ro.setResult(ResultDic.FAIL);
-            return ro;
-        }
-    }
+	/**
+	 * 删除产品规格编码
+	 *
+	 * @mbg.generated 自动生成，如需修改，请删除本行
+	 */
+	@DeleteMapping("/prd/productspeccode")
+	Ro del(@RequestParam("id") java.lang.Long id) {
+		_log.info("del PrdProductSpecCodeMo by id: {}", id);
+		int result = svc.del(id);
+		Ro ro = new Ro();
+		if (result == 1) {
+			String msg = "删除成功";
+			_log.info("{}: id-{}", msg, id);
+			ro.setMsg(msg);
+			ro.setResult(ResultDic.SUCCESS);
+			return ro;
+		} else {
+			String msg = "删除失败，找不到该记录";
+			_log.error("{}: id-{}", msg, id);
+			ro.setMsg(msg);
+			ro.setResult(ResultDic.FAIL);
+			return ro;
+		}
+	}
 
-    /**
-     * 查询产品规格编码
-     *
-     * @mbg.generated 自动生成，如需修改，请删除本行
-     */
-    @GetMapping("/prd/productspeccode")
-    PageInfo<PrdProductSpecCodeMo> list(PrdProductSpecCodeMo mo, @RequestParam(value = "pageNum", required = false) Integer pageNum, @RequestParam(value = "pageSize", required = false) Integer pageSize) {
-        if (pageNum == null)
-            pageNum = 1;
-        if (pageSize == null)
-            pageSize = 5;
-        _log.info("list PrdProductSpecCodeMo:" + mo + ", pageNum = " + pageNum + ", pageSize = " + pageSize);
-        if (pageSize > 50) {
-            String msg = "pageSize不能大于50";
-            _log.error(msg);
-            throw new IllegalArgumentException(msg);
-        }
-        PageInfo<PrdProductSpecCodeMo> result = svc.list(mo, pageNum, pageSize);
-        _log.info("result: " + result);
-        return result;
-    }
+	/**
+	 * 查询产品规格编码
+	 *
+	 * @mbg.generated 自动生成，如需修改，请删除本行
+	 */
+	@GetMapping("/prd/productspeccode")
+	PageInfo<PrdProductSpecCodeMo> list(PrdProductSpecCodeMo mo,
+			@RequestParam(value = "pageNum", required = false) Integer pageNum,
+			@RequestParam(value = "pageSize", required = false) Integer pageSize) {
+		if (pageNum == null)
+			pageNum = 1;
+		if (pageSize == null)
+			pageSize = 5;
+		_log.info("list PrdProductSpecCodeMo:" + mo + ", pageNum = " + pageNum + ", pageSize = " + pageSize);
+		if (pageSize > 50) {
+			String msg = "pageSize不能大于50";
+			_log.error(msg);
+			throw new IllegalArgumentException(msg);
+		}
+		PageInfo<PrdProductSpecCodeMo> result = svc.list(mo, pageNum, pageSize);
+		_log.info("result: " + result);
+		return result;
+	}
 
-    /**
-     * 获取单个产品规格编码
-     *
-     * @mbg.generated 自动生成，如需修改，请删除本行
-     */
-    @GetMapping("/prd/productspeccode/getbyid")
-    PrdProductSpecCodeMo getById(@RequestParam("id") java.lang.Long id) {
-        _log.info("get PrdProductSpecCodeMo by id: {}", id);
-        return svc.getById(id);
-    }
+	/**
+	 * 获取单个产品规格编码
+	 *
+	 * @mbg.generated 自动生成，如需修改，请删除本行
+	 */
+	@GetMapping("/prd/productspeccode/getbyid")
+	PrdProductSpecCodeMo getById(@RequestParam("id") java.lang.Long id) {
+		_log.info("get PrdProductSpecCodeMo by id: {}", id);
+		return svc.getById(id);
+	}
+
+	/**
+	 * 校验规格编码是否存在
+	 * 
+	 * @param mo
+	 * @return
+	 */
+	@GetMapping("/prd/productspeccode/exist")
+	Ro exist(PrdProductSpecCodeMo mo) {
+		_log.info("exist PrdProductSpecCodeMo：{}", mo);
+		Ro ro = new Ro();
+		if (mo == null) {
+			ro.setResult(ResultDic.PARAM_ERROR);
+			ro.setMsg("参数不正确");
+			return ro;
+		}
+
+		boolean existFlag = svc.existSelective(mo);
+		if (existFlag) {
+			ro.setResult(ResultDic.FAIL);
+			ro.setMsg("商品条码不存在");
+			return ro;
+		}
+
+		ro.setResult(ResultDic.SUCCESS);
+		ro.setMsg("商品条码已存在");
+		return ro;
+	}
 }
