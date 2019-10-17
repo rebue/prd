7fed5fb101b411e5fdf608e4d4765a2bfa64c178
you <601559210@qq.com>
2019-10-17 10:58:39 +0800

    添加feign

---
+++ prd-svr-feign/src/main/java/rebue/prd/svr/feign/PrdProductSpecCodeSvc.java
@@ -0,0 +1,17 @@
+package rebue.prd.svr.feign;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import rebue.prd.mo.PrdProductSpecCodeMo;
+import rebue.sbs.feign.FeignConfig;
+
+@FeignClient(name = "prd-svr", configuration = FeignConfig.class, contextId = "prd-svr-product-spec-code")
+public interface PrdProductSpecCodeSvc {
+
+    @GetMapping("/prd/productspeccode/select-by-code")
+    public List<PrdProductSpecCodeMo> selectByCode(String code);
+
+}
